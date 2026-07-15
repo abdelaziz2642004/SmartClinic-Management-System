8f293b18b7d059b0b70ea0c54ae5330bf5b49dba
Margreet-Emil <147156878+Margreet-Emil@users.noreply.github.com>
2026-07-15 03:03:27 +0300

    Add files via upload

---
+++ Migrations/20260712112811_AddAppointmentCountStoredProcedure.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clinic.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAppointmentCountStoredProcedure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                CREATE PROCEDURE GetAppointmentCountPerDoctor
+                AS
+                BEGIN
+                    SELECT 
+                        d.Id AS DoctorId,
+                        d.Name AS DoctorName,
+                        COUNT(a.AppointmentId) AS TotalAppointments
+                    FROM Doctors d
+                    LEFT JOIN Appointments a ON a.DoctorId = d.Id
+                    GROUP BY d.Id, d.Name
+                END
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP PROCEDURE GetAppointmentCountPerDoctor");
+        }
+    }
+}
